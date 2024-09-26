6a52cb82d52ddd057637c6a0cd19141669bad03f
Alvaro Gomez <a.gomez191@usp.ceu.es>
2024-09-26 20:46:58 +0200

    Changed fronted to React + TS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to Fast Blog</h1>
+      <div className="card">
+        <p className="read-the-docs">Every post is a Markdown file.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
